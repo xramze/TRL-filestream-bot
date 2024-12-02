1a5835a7352c7cc142734f18d46206228c086fd0
Techify Bots <RahulReviewsYT@gmail.com>
2024-12-02 11:43:51 +0530

    Create __init__.py

---
+++ biisal/bot/__init__.py
@@ -0,0 +1,17 @@
+# (c) TechifyBots (c) biisal (c) adarsh-goel
+from pyrogram import Client
+from ..vars import Var
+from os import getcwd
+
+StreamBot = Client(
+    name='Web Streamer',
+    api_id=Var.API_ID,
+    api_hash=Var.API_HASH,
+    bot_token=Var.BOT_TOKEN,
+    sleep_threshold=Var.SLEEP_THRESHOLD,
+    workers=Var.WORKERS
+)
+
+multi_clients = {}
+work_loads = {}
+
