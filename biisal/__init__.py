80515653ce1944a1547b8afe1d8747629d621032
Techify Bots <RahulReviewsYT@gmail.com>
2024-12-02 11:44:57 +0530

    Add files via upload

---
+++ biisal/__init__.py
@@ -0,0 +1,6 @@
+# (c) @adarsh-goel (c) @biisal (c) TechifyBots
+
+
+import time
+StartTime = time.time()
+__version__ = 1.1
---
+++ biisal/vars.py
@@ -0,0 +1,53 @@
+# (c) adarsh-goel (c) biisal (c) TechifyBots
+import os
+from os import getenv, environ
+from dotenv import load_dotenv
+
+def is_enabled(value, default):
+    if value.lower() in ["true", "yes", "1", "enable", "y"]:
+        return True
+    elif value.lower() in ["false", "no", "0", "disable", "n"]:
+        return False
+    else:
+        return default
+
+load_dotenv()
+
+class Var(object):
+    MULTI_CLIENT = False
+    API_ID = int(getenv('API_ID', ''))
+    API_HASH = str(getenv('API_HASH', ''))
+    BOT_TOKEN = str(getenv('BOT_TOKEN' , ''))
+    PICS = (environ.get('PICS', 'https://envs.sh/jUp.jpg')).split()
+    name = str(getenv('name', 'linkstreamrobot'))
+    SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
+    WORKERS = int(getenv('WORKERS', '4'))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', ''))
+    NEW_USER_LOG = int(getenv('NEW_USER_LOG', ''))
+    PORT = int(getenv('PORT', '8080'))
+    BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
+    PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
+    OWNER_ID = [int(x) for x in os.environ.get("OWNER_ID", "").split()]
+    NO_PORT = bool(getenv('NO_PORT', False))
+    APP_NAME = None
+    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'TechifyGuru'))
+    if 'DYNO' in environ:
+        ON_HEROKU = True
+        APP_NAME = str(getenv('APP_NAME')) #dont need to fill anything here
+    
+    else:
+        ON_HEROKU = False
+    FQDN = str(getenv('FQDN', 'BIND_ADRESS:PORT')) if not ON_HEROKU or getenv('FQDN', '') else APP_NAME+'.herokuapp.com'
+    HAS_SSL=bool(getenv('HAS_SSL',True))
+    if HAS_SSL:
+        URL = "https://{}/".format(FQDN)
+    else:
+        URL = "http://{}/".format(FQDN)
+    DATABASE_URL = str(getenv('DATABASE_URL', ''))
+    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'TechifyBots')) 
+    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "")).split()))   
+    BAN_CHNL = list(set(int(x) for x in str(getenv("BAN_CHNL", "")).split()))   
+    BAN_ALERT = str(getenv('BAN_ALERT' , '<b>ʏᴏᴜʀ ᴀʀᴇ ʙᴀɴɴᴇᴅ ᴛᴏ ᴜsᴇ ᴛʜɪs ʙᴏᴛ.ᴄᴏɴᴛᴀᴄᴛ @CallOwnerBot ᴛᴏ ʀᴇsᴏʟᴠᴇ ᴛʜᴇ ɪssᴜᴇ!!</b>'))
+    SHORTLINK = is_enabled('SHORTLINK', False)
+    SHORTLINK_URL = getenv('SHORTLINK_URL', '')
+    SHORTLINK_API = getenv('SHORTLINK_API', '')
