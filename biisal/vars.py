99f376db6fe9872435efe76d9755d024635d23e8
Techify Bots <RahulReviewsYT@gmail.com>
2024-12-25 14:14:57 +0530

    Update vars.py

--- biisal/vars.py
+++ biisal/vars.py
@@ -30,7 +30,7 @@ class Var(object):
     OWNER_ID = [int(x) for x in os.environ.get("OWNER_ID", "").split()]
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
-    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'TechifyGuru'))
+    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'TechifyRahul'))
     if 'DYNO' in environ:
         ON_HEROKU = True
         APP_NAME = str(getenv('APP_NAME')) #dont need to fill anything here
